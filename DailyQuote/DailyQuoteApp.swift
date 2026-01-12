e0f3373b64f285ebc16e9357fda7f4b0190b801a
Fardeen Shahi <fardeenshahi786@gmail.com>
2026-01-12 23:40:54 +0530

    Initial Commit

---
+++ DailyQuote/DailyQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuoteApp.swift
+//  DailyQuote
+//
+//  Created by Fardeen Shahi on 12/01/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
